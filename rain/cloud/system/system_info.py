1cc43cd9b3a487b128a1101cfdb3c7fad7b60257
SuPerC <panguangchao@outlook.com>
2018-12-23 23:09:55 +0800

    Stripping collection disk information

--- rain/cloud/system/system_info.py
+++ rain/cloud/system/system_info.py
@@ -13,8 +13,8 @@ from rain.common import utils
 class SystemInfo(object):
     """system information.
 
-    Collect system information, including cpu, memory, hard disk, network,
-    host name, boot time, login information, process...
+    Collect system information, including cpu, memory, hostname, boot time,
+    login information...
     """
 
     def _load_stat(self):
@@ -72,78 +72,78 @@ class SystemInfo(object):
         }
         return memcache_info_dict
 
-    def _get_phy_disk(self, disk_info):
-        """Return to the physical disk dictionary,
-        each key corresponds to the disk Partitions.
-        """
-        physical_disk_dict = {}
-        for dev_name in disk_info.keys():
-            if disk_info[dev_name]['Type'] == 'Device' \
-                    and 'cdrom' not in str(disk_info[dev_name]):
-                physical_disk_dict[dev_name] = \
-                    disk_info[dev_name]['Partitions']
-        try:
-            root_device = \
-                disk_info['/dev/mapper/centos-root']['HostDevice']
-            root_partition = \
-                disk_info['/dev/mapper/centos-root']['HostPartition']
-            index = physical_disk_dict[root_device].index(root_partition)
-            physical_disk_dict[root_device][index] = \
-                '/dev/mapper/centos-root'
-            physical_disk_dict = utils.byteify(physical_disk_dict)
-        except KeyError:
-            pass
-        return physical_disk_dict
+    # def _get_phy_disk(self, disk_info):
+    #     """Return to the physical disk dictionary,
+    #     each key corresponds to the disk Partitions.
+    #     """
+    #     physical_disk_dict = {}
+    #     for dev_name in disk_info.keys():
+    #         if disk_info[dev_name]['Type'] == 'Device' \
+    #                 and 'cdrom' not in str(disk_info[dev_name]):
+    #             physical_disk_dict[dev_name] = \
+    #                 disk_info[dev_name]['Partitions']
+    #     try:
+    #         root_device = \
+    #             disk_info['/dev/mapper/centos-root']['HostDevice']
+    #         root_partition = \
+    #             disk_info['/dev/mapper/centos-root']['HostPartition']
+    #         index = physical_disk_dict[root_device].index(root_partition)
+    #         physical_disk_dict[root_device][index] = \
+    #             '/dev/mapper/centos-root'
+    #         physical_disk_dict = utils.byteify(physical_disk_dict)
+    #     except KeyError:
+    #         pass
+    #     return physical_disk_dict
 
-    def get_disk_info(self):
-        """Get the physical hard disk information, The return dictionary
-        contains hard disk usage information and partition information.
-        """
-        # Need to add multi-threaded or asynchronous.
-        disk_info = []
-        # Get all device information.
-        all_dev_info = getdevinfo.get_info()
-        # Return to the list of physical disks.
-        # {'/dev/sda': ['/dev/sda1', '/dev/sda2', '/dev/mapper/centos-root']}
-        physical_disk_dict = self._get_phy_disk(all_dev_info)
-        for physical_disk, disk_partitions in physical_disk_dict.items():
-            single_disk_info = {}
-            parts_info = []
-            disk_used = 0
-            GB = int(1024 ** 3)
-            disk_capacity = \
-                all_dev_info[physical_disk]['Capacity'].strip(' GB')
-            disk_product = all_dev_info[physical_disk]['Product']
-            # Get each disk partition information.
-            for partitions in disk_partitions:
-                for psutil_partitions in psutil.disk_partitions():
-                    if partitions == psutil_partitions.device and \
-                            'docker' not in psutil_partitions.mountpoint:
-                        part_mountpoint = psutil_partitions.mountpoint
-                        part_usage = psutil.disk_usage(part_mountpoint)
-                        part_info = {
-                            'part_mountpoint': psutil_partitions.mountpoint,
-                            'part_fstype': psutil_partitions.fstype,
-                            'part_opts': psutil_partitions.opts,
-                            'part_total': part_usage.total / GB,
-                            'part_used': part_usage.used / GB,
-                            'part_free': part_usage.free / GB,
-                            'part_percent': part_usage.percent / GB
-                        }
-                        parts_info.append(part_info)
-            # Count the space used by a single disk.
-            for part_count in parts_info:
-                disk_used += part_count['part_used']
-            single_disk_info['disk_capacity(GB)'] = disk_capacity
-            single_disk_info['disk_used(GB)'] = disk_used
-            disk_percent = format(float(disk_used)/float(
-                disk_capacity), '.2f')
-            single_disk_info['disk_percent'] = disk_percent
-            single_disk_info['disk_product'] = disk_product
-            single_disk_info['disk_part_info'] = parts_info
-            disk_info.append(single_disk_info)
-            disk_info = utils.byteify(disk_info)
-        return disk_info
+    # def get_disk_info(self):
+    #     """Get the physical hard disk information, The return dictionary
+    #     contains hard disk usage information and partition information.
+    #     """
+    #     # Need to add multi-threaded or asynchronous.
+    #     disk_info = []
+    #     # Get all device information.
+    #     all_dev_info = getdevinfo.get_info()
+    #     # Return to the list of physical disks.
+    #     # {'/dev/sda': ['/dev/sda1', '/dev/sda2', '/dev/mapper/centos-root']}
+    #     physical_disk_dict = self._get_phy_disk(all_dev_info)
+    #     for physical_disk, disk_partitions in physical_disk_dict.items():
+    #         single_disk_info = {}
+    #         parts_info = []
+    #         disk_used = 0
+    #         GB = int(1024 ** 3)
+    #         disk_capacity = \
+    #             all_dev_info[physical_disk]['Capacity'].strip(' GB')
+    #         disk_product = all_dev_info[physical_disk]['Product']
+    #         # Get each disk partition information.
+    #         for partitions in disk_partitions:
+    #             for psutil_partitions in psutil.disk_partitions():
+    #                 if partitions == psutil_partitions.device and \
+    #                         'docker' not in psutil_partitions.mountpoint:
+    #                     part_mountpoint = psutil_partitions.mountpoint
+    #                     part_usage = psutil.disk_usage(part_mountpoint)
+    #                     part_info = {
+    #                         'part_mountpoint': psutil_partitions.mountpoint,
+    #                         'part_fstype': psutil_partitions.fstype,
+    #                         'part_opts': psutil_partitions.opts,
+    #                         'part_total': part_usage.total / GB,
+    #                         'part_used': part_usage.used / GB,
+    #                         'part_free': part_usage.free / GB,
+    #                         'part_percent': part_usage.percent / GB
+    #                     }
+    #                     parts_info.append(part_info)
+    #         # Count the space used by a single disk.
+    #         for part_count in parts_info:
+    #             disk_used += part_count['part_used']
+    #         single_disk_info['disk_capacity(GB)'] = disk_capacity
+    #         single_disk_info['disk_used(GB)'] = disk_used
+    #         disk_percent = format(float(disk_used)/float(
+    #             disk_capacity), '.2f')
+    #         single_disk_info['disk_percent'] = disk_percent
+    #         single_disk_info['disk_product'] = disk_product
+    #         single_disk_info['disk_part_info'] = parts_info
+    #         disk_info.append(single_disk_info)
+    #         disk_info = utils.byteify(disk_info)
+    #     return disk_info
 
     def get_boot_time(self):
         """Get system boot time and return.
