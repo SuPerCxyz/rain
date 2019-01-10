bc520eb54522a3128279c3d962432fc2cd7850a4
guangchao.pan <guangchao.pan@easystack.cn>
2019-01-10 20:10:15 +0800

    Structural optimization

--- rain/surface/socket_server.py
+++ rain/surface/socket_server.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 
-import os
 import socket
 import sys
-import time
 import threading
 
 from rain.common import rain_log
 from rain.config import default_conf
-from rain.surface import rain_celery
 
 CONF = default_conf.CONF
 logger = rain_log.logg(__name__)
@@ -31,7 +28,7 @@ class ScoketServer(object):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((address, port))
-            s.listen(10)
+            s.listen(100)
         except socket.error as msg:
             logger.error(msg)
             sys.exit(1)
@@ -54,6 +51,5 @@ class ScoketServer(object):
                 logger.info('Disconnect from {}.'.format(addr))
                 conn.close()
                 break
-            # self._sned_to_mq(addr, recv)
             conn.send('Successfully received data.')
             logger.info('Successfully received from {}.'.format(addr))
