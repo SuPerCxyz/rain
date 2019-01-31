3b62d5a5426bc3dbddaf2e12c645927929770f0e
guangchao.pan <guangchao.pan@easystack.cn>
2019-01-31 19:04:57 +0800

    fixifxifx

--- client.py
+++ client.py
@@ -9,20 +9,18 @@ from rain.cloud import sum_info
 from rain.cloud import socket_client
 
 
-@func_set_timeout(60)
-def get_data():
-    try:
-        sum_init = sum_info.SumInfo()
-        socket_cl = socket_client.SocketClient()
-        result = sum_init.sum_info()
-        socket_cl.send_data(result)
-    except Exception as e:
-        print e
-
-
 def client():
     while True:
         try:
+            @func_set_timeout(60)
+            def get_data():
+                try:
+                    sum_init = sum_info.SumInfo()
+                    socket_cl = socket_client.SocketClient()
+                    result = sum_init.sum_info()
+                    socket_cl.send_data(result)
+                except Exception as e:
+                    print e
             get_data()
             time.sleep(30)
         except Exception as e:
